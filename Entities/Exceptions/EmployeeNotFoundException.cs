20704df9a187f57d683aca6a0e114b1bfacd615e
umayer2017331016 <44854824+umayer2017331016@users.noreply.github.com>
2025-12-03 07:30:21 +0600

    .

---
+++ Entities/Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Exceptions
+{
+    public sealed class EmployeeNotFoundException : NotFoundException
+    {
+        public EmployeeNotFoundException(Guid employeeId)
+    : base($"Employee with id: {employeeId} doesn't exist in the database.")
+        {
+        }
+    }
+}

9ea17d783bbb15718edd1c9e5454ea590468ee8b
umayer2017331016 <44854824+umayer2017331016@users.noreply.github.com>
2025-12-03 17:12:40 +0600

    .

--- Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20704df9a187f57d683aca6a0e114b1bfacd615e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5489f32e7c452d0b498e620ead994305cff5c64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
